cd6d1de7bce85b8a2823a2e42f320bde80f059e5
Alan Wolfe <alan.wolfe@gmail.com>
2022-07-09 11:57:23 -0700

    progress on reporting non 2d data

--- main.cpp
+++ main.cpp
@@ -9,6 +9,223 @@ using Mtx = BaseMtx<double, WIDTH, HEIGHT>;
 template <size_t SIZE>
 using Vec = BaseVec<double, SIZE>;
 
+// ND data reporting
+template <size_t DATA_WIDTH, size_t DATA_HEIGHT>
+void Report(
+    const Mtx<DATA_WIDTH, DATA_HEIGHT>& data,
+    const Mtx<DATA_WIDTH, DATA_HEIGHT> recoveredData[],
+    const Vec<DATA_WIDTH>& eigenValues,
+    const std::array<Vec<DATA_WIDTH>, DATA_WIDTH>& eigenVectors,
+    const Vec<DATA_WIDTH>& RMSE,
+    const char* fileNameBase)
+{
+    // TODO: For each component count:
+    //  measure error for each data point
+    //  Report RMSE
+    //  Report eigenvectors and eigenvalues
+    // TODO: use this to analyze box, gauss, the 3d exmample form that website. what else?
+
+    printf("%s.txt\n", fileNameBase);
+
+    char fileName[1024];
+    sprintf_s(fileName, "out/%s.txt", fileNameBase);
+    FILE* file = nullptr;
+    fopen_s(&file, fileName, "w+t");
+
+    fprintf(file, "Eigen values and eigen vectors\n\n");
+    for (int i = 0; i < DATA_WIDTH; ++i)
+    {
+        fprintf(file, "[%f] [", eigenValues[i]);
+        for (int j = 0; j < DATA_WIDTH; ++j)
+        {
+            if (j > 0)
+                fprintf(file, ", %f", eigenVectors[i][j]);
+            else
+                fprintf(file, "%f", eigenVectors[i][j]);
+        }
+        fprintf(file, "]\n");
+    }
+    fprintf(file, "\nRMSE for component counts:\n");
+
+    for (int componentCount = 0; componentCount < DATA_WIDTH; ++componentCount)
+        fprintf(file, "[%i] %f\n", componentCount + 1, RMSE[componentCount]);
+    fprintf(file, "\n");
+
+
+    fprintf(file, "Data:\n");
+    for (int i = 0; i < DATA_HEIGHT; ++i)
+    {
+        fprintf(file, "[");
+        for (int j = 0; j < DATA_WIDTH; ++j)
+        {
+            if (j > 0)
+                fprintf(file, ", %f", data[i][j]);
+            else
+                fprintf(file, "%f", data[i][j]);
+        }
+        fprintf(file, "]\n");
+    }
+
+    for (int componentCount = 0; componentCount < DATA_WIDTH; ++componentCount)
+    {
+        if (eigenValues[componentCount] == 0.0f)
+            continue;
+
+        fprintf(file, "\n========================\n%i Components\n========================\n\n", componentCount + 1);
+
+
+        for (int i = 0; i < DATA_HEIGHT; ++i)
+        {
+            fprintf(file, "[");
+            for (int j = 0; j < DATA_WIDTH; ++j)
+            {
+                if (j > 0)
+                    fprintf(file, ", %f", recoveredData[componentCount][i][j]);
+                else
+                    fprintf(file, "%f", recoveredData[componentCount][i][j]);
+            }
+            fprintf(file, "]\n");
+        }
+    }
+
+    fclose(file);
+}
+
+// 2D data reporting
+template <size_t DATA_HEIGHT>
+void Report(
+    const Mtx<2, DATA_HEIGHT>& data,
+    const Mtx<2, DATA_HEIGHT> recoveredData[],
+    const Vec<2>& eigenValues,
+    const std::array<Vec<2>, 2>& eigenVectors,
+    const Vec<2>& RMSE,
+    const char* fileNameBase)
+{
+    static const size_t DATA_WIDTH = 2;
+
+    // get the min and max of the recovered data, to frame the graphs
+    Vec<DATA_WIDTH> rdmin, rdmax;
+    std::fill(rdmin.begin(), rdmin.end(), FLT_MAX);
+    std::fill(rdmax.begin(), rdmax.end(), -FLT_MAX);
+    for (int componentCount = 0; componentCount < DATA_WIDTH; ++componentCount)
+    {
+        if (eigenValues[componentCount] == 0.0f)
+            continue;
+
+        for (int rowIndex = 0; rowIndex < DATA_HEIGHT; ++rowIndex)
+        {
+            for (int columnIndex = 0; columnIndex < DATA_WIDTH; ++columnIndex)
+            {
+                rdmin[columnIndex] = std::min(rdmin[columnIndex], recoveredData[componentCount][rowIndex][columnIndex]);
+                rdmin[columnIndex] = std::min(rdmin[columnIndex], data[rowIndex][columnIndex]);
+
+                rdmax[columnIndex] = std::max(rdmax[columnIndex], recoveredData[componentCount][rowIndex][columnIndex]);
+                rdmax[columnIndex] = std::max(rdmax[columnIndex], data[rowIndex][columnIndex]);
+            }
+        }
+    }
+
+    // expand the min/max box a bit
+    for (int columnIndex = 0; columnIndex < DATA_WIDTH; ++columnIndex)
+    {
+        double mid = (rdmin[columnIndex] + rdmax[columnIndex]) / 2.0f;
+        double halfWidth = (rdmax[columnIndex] - rdmin[columnIndex]) / 2.0f;
+        rdmin[columnIndex] = mid - halfWidth * 1.1f;
+        rdmax[columnIndex] = mid + halfWidth * 1.1f;
+    }
+
+    // spit out the python scripts to make graphs
+    char fileName[1024];
+    for (int componentCount = 0; componentCount < DATA_WIDTH; ++componentCount)
+    {
+        if (eigenValues[componentCount] == 0.0f)
+            continue;
+
+        printf("%s.%i.py\n", fileNameBase, componentCount + 1);
+
+        sprintf_s(fileName, "out/%s.%i.py", fileNameBase, componentCount + 1);
+        FILE* file = nullptr;
+        fopen_s(&file, fileName, "w+t");
+        fprintf(file,
+            "import matplotlib.pyplot as plt\n"
+            "import numpy as np\n"
+            "\n"
+        );
+
+        // Print out eigen values and eigen vectors
+        fprintf(file, "# Eigen Values & Eigen (Principle Component) Vectors:\n");
+        for (int eigenIndex = 0; eigenIndex <= componentCount; ++eigenIndex)
+        {
+            fprintf(file, "# [%f] : [", eigenValues[eigenIndex]);
+            for (int componentIndex = 0; componentIndex < eigenVectors[eigenIndex].size(); ++componentIndex)
+            {
+                if (componentIndex > 0)
+                    fprintf(file, ", %f", eigenVectors[eigenIndex][componentIndex]);
+                else
+                    fprintf(file, "%f", eigenVectors[eigenIndex][componentIndex]);
+            }
+            fprintf(file, "]\n");
+        }
+        fprintf(file, "\n");
+
+        // data
+        for (int columnIndex = 0; columnIndex < DATA_WIDTH; ++columnIndex)
+        {
+            fprintf(file, "data%i = [", columnIndex);
+            for (int rowIndex = 0; rowIndex < DATA_HEIGHT; ++rowIndex)
+            {
+                if (rowIndex > 0)
+                    fprintf(file, ", %f", data[rowIndex][columnIndex]);
+                else
+                    fprintf(file, "%f", data[rowIndex][columnIndex]);
+            }
+            fprintf(file, "]\n");
+        }
+
+        // recoveredData
+        fprintf(file, "\n");
+        for (int columnIndex = 0; columnIndex < DATA_WIDTH; ++columnIndex)
+        {
+            fprintf(file, "recoveredData%i = [", columnIndex);
+            for (int rowIndex = 0; rowIndex < DATA_HEIGHT; ++rowIndex)
+            {
+                if (rowIndex > 0)
+                    fprintf(file, ", %f", recoveredData[componentCount][rowIndex][columnIndex]);
+                else
+                    fprintf(file, "%f", recoveredData[componentCount][rowIndex][columnIndex]);
+            }
+            fprintf(file, "]\n");
+        }
+
+        fprintf(file,
+            "\n"
+            "fig = plt.figure()\n"
+            "\n"
+            "plt.scatter(data0, data1, label=\"Points\")\n"
+            "plt.scatter(recoveredData0, recoveredData1, marker=\"+\", label=\"PCA Points\")\n"
+            "\n"
+        );
+        fprintf(file,
+            "plt.title(\"%i of %i PCA Components\\nRMSE = %f\")\n"
+            "plt.xlim([%f, %f])\n"
+            "plt.ylim([%f, %f])\n"
+            "plt.legend()\n"
+            "plt.tight_layout()\n"
+            "\n",
+            componentCount + 1, (int)DATA_WIDTH,
+            RMSE[componentCount],
+            rdmin[0], rdmax[0],
+            rdmin[1], rdmax[1]
+        );
+        fprintf(file, "fig.savefig(\"out/%s.%i.png\")\n", fileNameBase, componentCount + 1);
+        fclose(file);
+
+        // run the python file
+        sprintf_s(fileName, "python out/%s.%i.py", fileNameBase, componentCount + 1);
+        system(fileName);
+    }
+}
+
 template <size_t DATA_WIDTH, size_t DATA_HEIGHT>
 void DoTest(Mtx<DATA_WIDTH, DATA_HEIGHT> data, const char* fileNameBase, bool centerData)
 {
@@ -110,143 +327,33 @@ void DoTest(Mtx<DATA_WIDTH, DATA_HEIGHT> data, const char* fileNameBase, bool ce
         RMSE[componentCount] = sqrt(RMSE[componentCount]);
     }
 
-    // get the min and max of the recovered data, to frame the graphs
-    Vec<DATA_WIDTH> rdmin, rdmax;
-    std::fill(rdmin.begin(), rdmin.end(), FLT_MAX);
-    std::fill(rdmax.begin(), rdmax.end(), -FLT_MAX);
-    for (int componentCount = 0; componentCount < Columns(WT); ++componentCount)
-    {
-        if (eigenValues[componentCount] == 0.0f)
-            continue;
-
-        for (int rowIndex = 0; rowIndex < DATA_HEIGHT; ++rowIndex)
-        {
-            for (int columnIndex = 0; columnIndex < DATA_WIDTH; ++columnIndex)
-            {
-                rdmin[columnIndex] = std::min(rdmin[columnIndex], recoveredData[componentCount][rowIndex][columnIndex]);
-                rdmin[columnIndex] = std::min(rdmin[columnIndex], data[rowIndex][columnIndex]);
-
-                rdmax[columnIndex] = std::max(rdmax[columnIndex], recoveredData[componentCount][rowIndex][columnIndex]);
-                rdmax[columnIndex] = std::max(rdmax[columnIndex], data[rowIndex][columnIndex]);
-            }
-        }
-    }
-
-    // expand the min/max box a bit
-    for (int columnIndex = 0; columnIndex < DATA_WIDTH; ++columnIndex)
-    {
-        double mid = (rdmin[columnIndex] + rdmax[columnIndex]) / 2.0f;
-        double halfWidth = (rdmax[columnIndex] - rdmin[columnIndex]) / 2.0f;
-        rdmin[columnIndex] = mid - halfWidth * 1.1f;
-        rdmax[columnIndex] = mid + halfWidth * 1.1f;
-    }
-
-    // spit out the python scripts to make graphs
-    char fileName[1024];
-    for (int componentCount = 0; componentCount < Columns(WT); ++componentCount)
-    {
-        if (eigenValues[componentCount] == 0.0f)
-            continue;
-
-        printf("%s.%i.py\n", fileNameBase, componentCount + 1);
-
-        sprintf_s(fileName, "out/%s.%i.py", fileNameBase, componentCount + 1);
-        FILE* file = nullptr;
-        fopen_s(&file, fileName, "w+t");
-        fprintf(file,
-            "import matplotlib.pyplot as plt\n"
-            "import numpy as np\n"
-            "\n"
-        );
-
-        // Print out eigen values and eigen vectors
-        fprintf(file, "# Eigen Values & Eigen (Principle Component) Vectors:\n");
-        for (int eigenIndex = 0; eigenIndex <= componentCount; ++eigenIndex)
-        {
-            fprintf(file, "# [%f] : [", eigenValues[eigenIndex]);
-            for (int componentIndex = 0; componentIndex < eigenVectors[eigenIndex].size(); ++componentIndex)
-            {
-                if (componentIndex > 0)
-                    fprintf(file, ", %f", eigenVectors[eigenIndex][componentIndex]);
-                else
-                    fprintf(file, "%f", eigenVectors[eigenIndex][componentIndex]);
-            }
-            fprintf(file, "]\n");
-        }
-        fprintf(file, "\n");
-
-        // data
-        for (int columnIndex = 0; columnIndex < DATA_WIDTH; ++columnIndex)
-        {
-            fprintf(file, "data%i = [", columnIndex);
-            for (int rowIndex = 0; rowIndex < DATA_HEIGHT; ++rowIndex)
-            {
-                if (rowIndex > 0)
-                    fprintf(file, ", %f", data[rowIndex][columnIndex]);
-                else
-                    fprintf(file, "%f", data[rowIndex][columnIndex]);
-            }
-            fprintf(file, "]\n");
-        }
-
-        // recoveredData
-        fprintf(file, "\n");
-        for (int columnIndex = 0; columnIndex < DATA_WIDTH; ++columnIndex)
-        {
-            fprintf(file, "recoveredData%i = [", columnIndex);
-            for (int rowIndex = 0; rowIndex < DATA_HEIGHT; ++rowIndex)
-            {
-                if (rowIndex > 0)
-                    fprintf(file, ", %f", recoveredData[componentCount][rowIndex][columnIndex]);
-                else
-                    fprintf(file, "%f", recoveredData[componentCount][rowIndex][columnIndex]);
-            }
-            fprintf(file, "]\n");
-        }
-
-        fprintf(file,
-            "\n"
-            "fig = plt.figure()\n"
-            "\n"
-            "plt.scatter(data0, data1, label=\"Points\")\n"
-            "plt.scatter(recoveredData0, recoveredData1, marker=\"+\", label=\"PCA Points\")\n"
-            "\n"
-        );
-        fprintf(file,
-            "plt.title(\"%i of %i PCA Components\\nRMSE = %f\")\n"
-            "plt.xlim([%f, %f])\n"
-            "plt.ylim([%f, %f])\n"
-            "plt.legend()\n"
-            "plt.tight_layout()\n"
-            "\n",
-            componentCount+1, (int)Columns(WT),
-            RMSE[componentCount],
-            rdmin[0], rdmax[0],
-            rdmin[1], rdmax[1]
-        );
-        fprintf(file, "fig.savefig(\"out/%s.%i.png\")\n", fileNameBase, componentCount + 1);
-        fclose(file);
-
-        // run the python file
-        sprintf_s(fileName, "python out/%s.%i.py", fileNameBase, componentCount + 1);
-        system(fileName);
-    }
+    Report(
+        data,
+        recoveredData,
+        eigenValues,
+        eigenVectors,
+        RMSE,
+        fileNameBase);
 }
 
 int main(int argc, char** argv)
 {
     _mkdir("out");
 
-    /*
-    Mtx<3, 5> data =
+    // test from the website
     {
-        90, 60, 90,
-        90, 90, 30,
-        60, 60, 60,
-        60, 60, 90,
-        30, 30, 30
-    };
-    */
+        Mtx<3, 5> data =
+        {
+            90, 60, 90,
+            90, 90, 30,
+            60, 60, 60,
+            60, 60, 90,
+            30, 30, 30
+        };
+
+        DoTest(data, "test3DU", false);
+        DoTest(data, "test3DC", true);
+    }
 
     // Basic data test
     {
